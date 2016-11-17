5e63e23fd7e46b9e99683495a138e3fe45c12386
János Illés <ijanos@gmail.com>
2016-11-17 20:35:23 +0100

    Better error messages with expect instead of unwrap

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,30 @@ fn strip_toolchain(name: &toml::Value) -> String {
 }
 
 fn read_settings() -> (String, BTreeMap<String, toml::Value>) {
-    let mut settings_path = env::home_dir().unwrap();
+    let mut settings_path = env::home_dir().expect("Cannot get home directory");
     settings_path.push(".multirust/settings.toml");
 
-    let mut file = File::open(&settings_path).expect("file open error");
+    let mut file = File::open(&settings_path).expect("Error opening settings.toml file");
 
     let mut buffer = String::new();
-    file.read_to_string(&mut buffer).expect("read error");
+    file.read_to_string(&mut buffer).expect("Error reading settings.toml");
 
-    let value = toml::Parser::new(&buffer).parse().unwrap();
-    let default = value.get("default_toolchain").unwrap().as_str().unwrap();
-    let overrides = value.get("overrides").unwrap().as_table().unwrap();
+    let value = toml::Parser::new(&buffer).parse().expect("Error parsing settings.toml");
+    let default = value.get("default_toolchain").expect("Did not found default_toolchain in settings.toml").as_str().unwrap();
+    let overrides = value.get("overrides").expect("Did not found overrides in settings.toml").as_table().unwrap();
 
     (default.to_owned(), overrides.to_owned())
 }
 
 fn main() {
-    let cwd = env::current_dir().unwrap();
+    let cwd = env::current_dir().expect("Cannot get working directory");
+    let cwd = cwd.to_str().expect("Cannot convert working directory to string");
     let (default, overrides) = read_settings();
 
-    let toolchain = match overrides.get(cwd.to_str().unwrap()) {
-        Some(toolchain) => strip_toolchain(toolchain),
-        None => default,
+    let toolchain = if let Some(toolchain) = overrides.get(cwd) {
+        strip_toolchain(toolchain)
+    } else {
+        default
     };
 
     println!("{}", toolchain);
